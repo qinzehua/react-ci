4c6da0dc5fdbad545c2fb53dc99763784381bd3a
qin.zehua <775935087@qq.com>
2021-07-24 16:25:23 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { Suspense } from "react";
+
+import {
+  atom,
+  selector,
+  selectorFamily,
+  useRecoilValue,
+  useRecoilState,
+  useRecoilValueLoadable,
+} from "recoil";
+import axios from "axios";
+
+const currentUserIDState = atom({
+  key: "CurrentUserID",
+  default: 0,
+});
+
+const userInfo = selectorFamily({
+  key: "userInfo",
+  get: (ID) => async () => {
+    const res = await axios.get("http://localhost:3001/users/" + ID);
+
+    if (res.error) {
+      throw res.error;
+    }
+    return res.data;
+  },
+});
+
+const currentUserInfoQuery = selector({
+  key: "currentUserInfoQuery",
+  get: ({ get }) => {
+    const info = get(userInfo(get(currentUserIDState)));
+    return info;
+  },
+});
+
+const friendsQuery = selector({
+  key: "friendsQuery",
+  get: ({ get }) => {
+    const { friends } = get(currentUserInfoQuery);
+    const list = friends.map((friendID) => get(userInfo(friendID)));
+    return list;
+  },
+});
+
+function CurrentUserInfo() {
+  const userStatus = useRecoilValueLoadable(currentUserInfoQuery);
+  const listStatus = useRecoilValueLoadable(friendsQuery);
+  const [, setId] = useRecoilState(currentUserIDState);
+  if (userStatus.state === "hasValue" && listStatus.state === "hasValue") {
+    const { name } = userStatus.contents;
+    const friends = listStatus.contents;
+
+    return (
+      <div>
+        current:{name}
+        <ul>
+          {friends.map((friend) => (
+            <li key={friend.id} onClick={() => setId(friend.id)}>
+              {friend.name}
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
+  } else {
+    return <div>loading</div>;
+  }
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CurrentUserInfo />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React, { Suspense } from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { RecoilRoot } from "recoil";
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {}
+
+  render() {
+    if (this.state.hasError) {
+      // 你可以自定义降级后的 UI 并渲染
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+  <RecoilRoot>
+    <ErrorBoundary>
+      <Suspense fallback={<div>loading...</div>}>
+        <React.StrictMode>
+          <h1>xxxxxx</h1>
+          <App />
+        </React.StrictMode>
+      </Suspense>
+    </ErrorBoundary>
+  </RecoilRoot>,
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  document.getElementById("root")
+);
